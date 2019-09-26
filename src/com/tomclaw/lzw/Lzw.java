05777846031573feb8fe7030bf1026fabaf84f44
Igor Solkin <inbox@tomclaw.com>
2019-09-26 10:03:40 +0300

    Initial commit

---
+++ src/com/tomclaw/lzw/Lzw.java
@@ -0,0 +1,73 @@
+package com.tomclaw.lzw;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Lzw {
+
+    public String lzw_compress(String input) {
+        HashMap<String, Integer> dictionary = new LinkedHashMap<>();
+        String[] data = (input + "").split("");
+        String out = "";
+        ArrayList<Character> temp_out = new ArrayList<>();
+        String currentChar;
+        String phrase = data[0];
+        int code = 256;
+        for (int i = 1; i < data.length; i++) {
+            currentChar = data[i];
+            if (dictionary.get(phrase + currentChar) != null) {
+                phrase += currentChar;
+            } else {
+                if (phrase.length() > 1) {
+                    temp_out.add((char) dictionary.get(phrase).intValue());
+                } else {
+                    temp_out.add((char) Character.codePointAt(phrase, 0));
+                }
+
+                dictionary.put(phrase + currentChar, code);
+                code++;
+                phrase = currentChar;
+            }
+        }
+
+        if (phrase.length() > 1) {
+            temp_out.add((char) dictionary.get(phrase).intValue());
+        } else {
+            temp_out.add((char) Character.codePointAt(phrase, 0));
+        }
+
+        for (char outchar : temp_out) {
+            out += outchar;
+        }
+        return out;
+    }
+
+    public String lzw_extract(String input) {
+        HashMap<Integer, String> dictionary = new LinkedHashMap<>();
+        String[] data = (input + "").split("");
+        String currentChar = data[0];
+        String oldPhrase = currentChar;
+        String out = currentChar;
+        int code = 256;
+        String phrase = "";
+        for (int i = 1; i < data.length; i++) {
+            int currCode = Character.codePointAt(data[i], 0);
+            if (currCode < 256) {
+                phrase = data[i];
+            } else {
+                if (dictionary.get(currCode) != null) {
+                    phrase = dictionary.get(currCode);
+                } else {
+                    phrase = oldPhrase + currentChar;
+                }
+            }
+            out += phrase;
+            currentChar = phrase.substring(0, 1);
+            dictionary.put(code, oldPhrase + currentChar);
+            code++;
+            oldPhrase = phrase;
+        }
+        return out;
+    }
+}
