04ba3f7f3dc74a1b65a464dbf20bc134b895365d
Igor Solkin <inbox@tomclaw.com>
2019-10-07 23:39:25 +0300

    Remove redundant code from main class

--- src/com/tomclaw/lzw/Main.java
+++ src/com/tomclaw/lzw/Main.java
@@ -9,40 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            ByteArrayInputStream input;
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-
-            File in = new File("/Users/solkin/Desktop/app-info.json");
-            byte[] data = Files.readAllBytes(in.toPath());
-            System.out.println(data.length + " bytes, (original)");
-
-            bytes.reset();
-            LZWOutputStream lzw = new LZWOutputStream(bytes);
-            lzw.write(data);
-            lzw.flush();
-            lzw.close();
-            System.out.println(bytes.size() + " bytes (encoded)");
-            File out = new File("/Users/solkin/Desktop/app-info.lzw");
-            OutputStream fos = new FileOutputStream(out);
-            fos.write(bytes.toByteArray());
-            fos.flush();
-            fos.close();
-
-            input = new ByteArrayInputStream(bytes.toByteArray());
-            bytes.reset();
-
-            LZWInputStream lzwInputStream = new LZWInputStream(input);
-            int read;
-            ByteArrayOutputStream sb = new ByteArrayOutputStream();
-            while ((read = lzwInputStream.read()) != -1) {
-                sb.write(read);
-            }
-            System.out.println(sb.size() + " bytes, (decoded)");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
             String command = args[0];
             String source = args[1];
             String destination = args[2];
@@ -77,6 +43,7 @@ public class Main {
                         while ((read = lzwInputStream.read()) != -1) {
                             decOutputStream.write(read);
                         }
+                        decOutputStream.flush();
                     } finally {
                         closeStream(lzwInputStream);
                         closeStream(encInputStream);
